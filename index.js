0efa86127a2e91926ad95f8fb93bba2156ab8441
Siddarth <55800552+thegeekboyy@users.noreply.github.com>
2020-09-08 20:29:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function button1(){
+    console.log("but 1");
+    const link3= document.getElementById("image");
+    link3.src= "https://cdn.hipwallpaper.com/i/50/46/XBLx2b.jpg";
+}
+
+
+
+
+function button2(){
+    console.log("but 2");
+    const link2 = document.getElementById("image");
+    link2.src="https://www.xtrafondos.com/wallpapers/sonic-como-flash-5196.jpg";
+}
+
+
+
+
+
+function button3(){
+    console.log("but 3");
+    const link3= document.getElementById("image");
+    link3.src= "https://wallpaperset.com/w/full/6/7/1/532447.jpg";
+}
